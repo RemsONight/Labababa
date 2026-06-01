63f3a3cccc7adef1c365888036b9b3a3a32f1dc6
RemsONight <roman9rems@gmail.com>
2026-06-01 09:07:15 +0700

    ff

---
+++ task.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+
+using namespace std; //hello hell!
+
+int main() {
+    double l, w;
+
+    printf("Enter Length: \n");
+    cin >> l;
+
+    printf("Enter Width: \n");
+    cin >> w;
+
+    if (l <= 0 || w <= 0) {
+        printf("Error: Unsupported numbers!");
+        return 1;
+    }
+
+    double perimeter = 2 * (l + w);
+    double area = l * w;
+    double diagonal = sqrt(l * l + w * w);
+
+    printf("\nResults:");
+    printf("perimeter: %.2f\n", perimeter);
+    printf("area: %.2f\n", area);
+    printf("diagonal: %.2f\n", diagonal);
+
+    return 0;
+}
+
